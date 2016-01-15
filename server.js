daa9a43e1bd44694ba9f302ab7b0957e5cedbb60
dsouzaedison <dsouzaedison11@gmail.com>
2016-01-14 16:42:35 -0800

    Update1

--- server.js
+++ server.js
@@ -5,23 +5,25 @@ var express = require('express');
 var app = express();
 var gpio= require('./gpiocontroller');
 app.locals.ledState = 0;
-var toggle,title="LED controller";
+var toggle,title="LED controller",color="#0f0";
 
 app.set('view engine','ejs');
 
 app.use(express.static(__dirname + '/views'));
 app.use(function(req,res,next){
     console.log('Request Recieved');
-    if(app.locals.ledState){
-        toggle="OFF";
-    }else{
-        toggle="ON";
+    if (app.locals.ledState) {
+        toggle = "OFF";
+        color = "#f00"
+    } else {
+        toggle = "ON";
+        color = "#0f0"
     }
     next();
 });
 
 app.get('/',function(req,res){
-    res.render('index',{title:title,toggle:toggle});
+    res.render('index',{title:title,toggle:toggle,color:color});
 });
 
 app.post('/toggle',function(req,res){
